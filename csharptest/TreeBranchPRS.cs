e7fa8aff3a6d50b28a28cc541faae1d6ad78ad6e
wkw <wkw@DESKTOP-RBVALPK>
2017-03-14 22:38:29 +0800

    init

---
+++ csharptest/TreeBranchPRS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptest
+{
+    class TreeBranchPRS
+    {
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public float PositionZ { get; set; }
+        public float RotationX { get; set; }
+        public float RotationY { get; set; }
+        public float RotationZ { get; set; }
+        public float ScaleX { get; set; }
+        public float ScaleY { get; set; }
+        public float ScaleZ { get; set; }
+        public LeafPRS[] Leaves;
+    }
+}
